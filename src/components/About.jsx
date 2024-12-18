bd8cbc3ea9078e586e2366e461820e1f5712f066
Dev Trivedi <dev.trivedi0523@gmail.com>
2024-12-18 22:28:56 +0530

    added channel name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -99,7 +99,7 @@ const About = () => {
           Hire Me
         </button> */}
         <a
-          href="src/assets/Dev Trivedi.pdf"  // Make sure this path is correct
+          href="public/Dev Trivedi.pdf"  // Make sure this path is correct
           download="Dev_Trivedi_Resume"
           className='px-6 py-2 bg-black-100 text-white rounded-full hover:bg-tertiary cursor pointer border border-secondary'
         >
